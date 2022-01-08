07927daf3faffbc4a2e09a14f140451345df4662
nickabramowicz@gmail.com <nickabramowicz@gmail.com>
2022-01-07 20:15:27 -0800

    DSA practice

---
+++ leetcode/arrays/plusOne.js
@@ -0,0 +1,22 @@
+// You are given a large integer represented as an integer array digits, where each digits[i] is the ith digit of the integer. 
+// The digits are ordered from most significant to least significant in left-to-right order. The large integer does not contain any leading 0's.
+// Increment the large integer by one and return the resulting array of digits.
+
+const plusOne = (digits) => {
+    for(let i = digits.length - 1; i >= 0; i--) {
+        if(digits[i] === 9) {
+            digits[i] = 0;
+        } else {
+            digits[i]++
+            return digits;
+        }
+    }
+    return [1, ...digits]
+};
+
+// iterate over array using for loop
+// start from end of array
+// if value is 9 then assign new value of 0
+// any other value increment by one
+// return digits immediately
+// outside of loop return 1 in front of our mutated array
