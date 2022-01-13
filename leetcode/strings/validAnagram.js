55480d1e8c9d76adaabf9ae825cfce6bd854f733
nickabramowicz@gmail.com <nickabramowicz@gmail.com>
2022-01-12 21:12:11 -0800

    DSA practice

---
+++ leetcode/strings/validAnagram.js
@@ -0,0 +1,26 @@
+// Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+// An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+
+const isAnagram = (s, t) => {
+    if(s.length !== t.length) {
+        return false;
+    }
+    
+    let map = {};
+    for(let i = 0; i < s.length; i++) {
+        if(!map[s[i]]) {
+            map[s[i]] = 1;
+        } else {
+            map[s[i]]++
+        }
+    }
+    
+    for(let i = 0; i < t.length; i++) {
+        if(!map[t[i]] || map[t[i]] < 0) {
+            return false;
+        } else {
+            map[t[i]]--
+        }
+    }
+    return true;
+};
