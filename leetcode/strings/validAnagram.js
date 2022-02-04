ab2d957d8e38eaf0b2669d92bcb634b193dae863
nickabramowicz@gmail.com <nickabramowicz@gmail.com>
2022-02-03 20:36:57 -0800

    DSA practice

--- leetcode/strings/validAnagram.js
+++ leetcode/strings/validAnagram.js
@@ -24,3 +24,29 @@ const isAnagram = (s, t) => {
     }
     return true;
 };
+
+// Alternative approach
+const isAnagram = (s, t) => {
+    if(s.length !== t.length) {
+        return false;
+    }
+    
+    let map = {};
+    
+    for(let i = 0; i < s.length; i++) {
+        if(!map[s[i]]) {
+            map[s[i]] = 1;
+        } else {
+            map[s[i]] += 1;
+        }
+    }
+    
+    for(let i = 0; i < t.length; i++) {
+        if(map[t[i]]) {
+            map[t[i]]--
+        } else {
+            return false;
+        }
+    }
+    return true;
+};
