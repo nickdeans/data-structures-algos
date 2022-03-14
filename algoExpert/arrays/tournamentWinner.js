c7a4f625b397444886f5639137b49cf635a633ba
nickabramowicz@gmail.com <nickabramowicz@gmail.com>
2022-03-13 23:04:18 -0700

    DSA practice

---
+++ algoExpert/arrays/tournamentWinner.js
@@ -0,0 +1,44 @@
+//   There's an algorithms tournament taking place in which teams of programmers
+//   compete against each other to solve algorithmic problems as fast as possible.
+//   Teams compete in a round robin, where each team faces off against all other
+//   teams. Only two teams compete against each other at a time, and for each
+//   competition, one team is designated the home team, while the other team is the
+//   away team. In each competition there's always one winner and one loser; there
+//   are no ties. A team receives 3 points if it wins and 0 points if it loses. The
+//   winner of the tournament is the team that receives the most amount of points.
+
+// Look at AlgoExpert for rest of question... too long to type
+
+const tournamentWinner = (competitions, results) => {
+    let winner = '';
+      let map = {};
+      let resultsIdx = 0;
+      let count = 0;
+      
+      for(let i = 0; i < competitions.length; i++) {
+          if(results[resultsIdx] === 1) {
+              if(!map[competitions[i][0]]) {
+                  map[competitions[i][0]] = 0
+              }
+              map[competitions[i][0]]++
+              resultsIdx++
+                  if(map[competitions[i][0]] > count) {
+                      winner = competitions[i][0]
+                      count = map[competitions[i][0]]
+                  }
+          } else {
+              if(!map[competitions[i][1]]) {
+                  map[competitions[i][1]] = 0;
+              }
+              map[competitions[i][1]]++
+              resultsIdx++
+                      if(map[competitions[i][1]] > count) {
+                      winner = competitions[i][1]
+                      count = map[competitions[i][1]]
+                  }
+          }
+      }
+      
+      return winner;
+  }
+  
